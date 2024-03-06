76fa3c7ed340be1aed12661e7ddafc0aea09365e
cssprad1 <caleb.s.spradlin@nasa.gov>
2024-03-06 14:56:56 -0500

    removed blocks of footprint-related code

--- view/demCreatorCLV.py
+++ view/demCreatorCLV.py
@@ -17,10 +17,6 @@ from evhr.model.DemCreatorCelery import DemCreatorCelery
 #
 # evhr/view/demCreatorCLV.py -t -o /explore/nobackup/people/rlgill/SystemTesting/testDEM3/ --scenes  '/css/nga/WV02/1B/2018/278/WV02_10300100889D0300_X1BS_502602073050_01/WV02_20181005212447_10300100889D0300_18OCT05212447-P1BS-502602073050_01_P002.ntf'
 #
-# evhr/view/demCreatorCLV.py -t -o /explore/nobackup/people/rlgill/SystemTesting/testDEM3/ -e -148 65 -147.5 64.5 4326
-#
-# evhr/view/demCreatorCLV.py -t -o /explore/nobackup/people/rlgill/SystemTesting/testDEM3/ --catIds 10300100889D0300
-#
 # -----------------------------------------------------------------------------
 def main():
 
@@ -79,6 +75,14 @@ def main():
 
     # ---
     # Scene pair list file
+    #
+    # Previous version of DemCreator took various identifiers which
+    # were then used to identify pairs through the footprints database.
+    #
+    # Now, users should directly supply the pairs which they want the
+    # dem created for in the form of a CSV where each row represents a
+    # pair. Col 1 is reserved for the pair name and the subsequent columns
+    # should be paths to scenes which make up the pair to be processed.
     # ---
     pairs = {}
 
